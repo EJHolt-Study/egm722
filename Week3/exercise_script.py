6a0d45688d79ad028c7dfebb642279979c4041a2
EJHolt-Study <Holt-E1@ulster.ac.uk>
2025-04-16 19:59:26 +0100

    Update exercise_script.py

--- Week3/exercise_script.py
+++ Week3/exercise_script.py
@@ -27,7 +27,7 @@ wc_combined = gpd.sjoin(counties_itm, wards_itm, how='inner', lsuffix='left', rs
 
 # Creating summarised GDF
 grouped_cnty_pop = wc_combined.groupby(['CountyName'])
-print(grouped_cnty_pop['Population'].sum())
+# print(grouped_cnty_pop['Population'].sum())
 
 
 
@@ -37,36 +37,36 @@ print(grouped_cnty_pop['Population'].sum())
 # print(counties_itm.crs)
 # print(roads_itm.crs)
 # ---------------------------------------------------------------------------------------------------------------------
-# # below here, you may need to modify the script somewhat to create your map.
-# # create a crs using ccrs.UTM() that corresponds to our CRS
-# ni_utm = ccrs.UTM(29)
-#
-# # create a figure of size 10x10 (representing the page size in inches)
-# fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=ni_utm))
-#
-# # add gridlines below
-# gridlines = ax.gridlines(draw_labels=True,
-#                          xlocs=[-8, -7.5, -7, -6.5, -6, -5.5],
-#                          ylocs=[54, 54.5, 55, 55.5])
-# gridlines.right_labels = False
-# gridlines.bottom_labels = False
-#
-# # to make a nice colorbar that stays in line with our map, use these lines:
-# divider = make_axes_locatable(ax)
-# cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
-#
-# # plot the ward data into our axis, using gdf.plot()
-# ward_plot = wards.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='viridis',
-#                        legend=True, cax=cax, legend_kwds={'label': 'Resident Population'})
-#
-# # add county outlines in red using ShapelyFeature
-# county_outlines = ShapelyFeature(counties['geometry'], ni_utm, edgecolor='r', facecolor='none')
-# ax.add_feature(county_outlines)
-#
-# county_handles = generate_handles([''], ['none'], edge='r')
-#
-# # add a legend in the upper left-hand corner
-# ax.legend(county_handles, ['County Boundaries'], fontsize=12, loc='upper left', framealpha=1)
-#
-# # save the figure
-# fig.savefig('sample_map.png', dpi=300, bbox_inches='tight')
+# below here, you may need to modify the script somewhat to create your map.
+# create a crs using ccrs.UTM() that corresponds to our CRS
+ni_utm = ccrs.UTM(29)
+
+# create a figure of size 10x10 (representing the page size in inches)
+fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=ni_utm))
+
+# add gridlines below
+gridlines = ax.gridlines(draw_labels=True,
+                        xlocs=[-8, -7.5, -7, -6.5, -6, -5.5],
+                        ylocs=[54, 54.5, 55, 55.5])
+gridlines.right_labels = False
+gridlines.bottom_labels = False
+
+# to make a nice colorbar that stays in line with our map, use these lines:
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
+
+# plot the ward data into our axis, using gdf.plot()
+ward_plot = wards.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='viridis',
+                        legend=True, cax=cax, legend_kwds={'label': 'Resident Population'})
+
+# add county outlines in red using ShapelyFeature
+county_outlines = ShapelyFeature(counties['geometry'], ni_utm, edgecolor='r', facecolor='none')
+ax.add_feature(county_outlines)
+
+county_handles = generate_handles([''], ['none'], edge='r')
+
+# add a legend in the upper left-hand corner
+ax.legend(county_handles, ['County Boundaries'], fontsize=12, loc='upper left', framealpha=1)
+
+# save the figure
+fig.savefig('created_sample_map.png', dpi=300, bbox_inches='tight')
